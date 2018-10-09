62799ef9d936e4e7ac93215019f30c574f15450c
davidcanonieto <david.cano.nieto@gmail.com>
2018-10-09 10:51:05 +0100

    [ADF-3617] Fix bugs in Task Details Page controller

--- e2e/pages/adf/process_services/taskDetailsPage.ts
+++ e2e/pages/adf/process_services/taskDetailsPage.ts
@@ -16,12 +16,12 @@
  */
 
 import { AppSettingsToggles } from './dialog/appSettingsToggles';
-import { TabsPage } from '../material/tabsPage';
-import { element, by, browser, protractor } from 'protractor';
 import Util = require('../../../util/util');
+import { element, by, protractor, browser } from 'protractor';
 
 export class TaskDetailsPage {
 
+    appSettingsTogglesClass = new AppSettingsToggles();
     formNameField = element(by.css('span[data-automation-id*="formName"] span'));
     assigneeField = element(by.css('span[data-automation-id*="assignee"] span'));
     statusField = element(by.css('span[data-automation-id*="status"] span'));
@@ -34,11 +34,14 @@ export class TaskDetailsPage {
     activitiesTitle = element(by.css('div[class*="adf-info-drawer-layout-header-title"] div'));
     commentField = element(by.id('comment-input'));
     addCommentButton = element(by.css('[data-automation-id="comments-input-add"]'));
+    activityTab = element(by.cssContainingText('div[class*="mat-tab-label"]', 'Activity'));
+    detailsTab = element(by.cssContainingText('div[class*="mat-tab-label""]', 'Details'));
     involvePeopleButton = element(by.css('div[class*="add-people"]'));
     addPeopleField = element(by.css('input[data-automation-id="adf-people-search-input"]'));
     addInvolvedUserButton = element(by.css('button[id="add-people"] span'));
     emailInvolvedUser = by.xpath('following-sibling::div[@class="people-email ng-star-inserted"]');
     editActionInvolvedUser = by.xpath('following-sibling::div[@class="people-edit-label ng-star-inserted"]');
+    involvedUserPic = by.xpath('ancestor::div/ancestor::div/preceding-sibling::div//div[@class="adf-people-search-people-pic ng-star-inserted"]');
     tadkDetailsInfoDrawer = element(by.tagName('adf-info-drawer'));
     taskDetailsSection = element(by.css('div[data-automation-id="adf-tasks-details"]'));
     taskDetailsEmptySection = element(by.css('div[data-automation-id="adf-tasks-details--empty"]'));
@@ -185,14 +188,24 @@ export class TaskDetailsPage {
     }
 
     selectActivityTab() {
-        let tabsPage = new TabsPage;
-        tabsPage.clickTabByTitle('Activity');
+        Util.waitUntilElementIsVisible(this.activityTab);
+        this.activityTab.getAttribute('aria-selected').then((check) => {
+            if (check === 'false') {
+                this.activityTab.click();
+                expect(this.activityTab.getAttribute('aria-selected') === 'true');
+            }
+        });
         return this;
     }
 
     selectDetailsTab() {
-        let tabsPage = new TabsPage;
-        tabsPage.clickTabByTitle('Details');
+        Util.waitUntilElementIsVisible(this.detailsTab);
+        this.detailsTab.getAttribute('aria-selected').then((check) => {
+            if (check === 'false') {
+                this.detailsTab.click();
+                expect(this.detailsTab.getAttribute('aria-selected') === 'true');
+            }
+        });
         return this;
     }
 
@@ -280,7 +293,7 @@ export class TaskDetailsPage {
     }
 
     appSettingsToggles() {
-        return new AppSettingsToggles();
+        return this.appSettingsTogglesClass;
     }
 
     taskInfoDrawerIsDisplayed() {
@@ -332,6 +345,12 @@ export class TaskDetailsPage {
     getInvolvedPeopleTitle() {
         Util.waitUntilElementIsVisible(this.peopleTitle);
         return this.peopleTitle.getText();
+    }
+
+    getInvolvedPeopleInitialImage(user) {
+        let pic = this.getRowsUser(user).element(this.involvedUserPic);
+        Util.waitUntilElementIsVisible(pic);
+        return pic.getText();
     }
 
     checkTaskDetails() {
